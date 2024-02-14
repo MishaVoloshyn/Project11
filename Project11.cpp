37760777500d4666696d0762668b2e968d72817c
MISHA <vol.mykhailo@gmail.com>
2024-02-14 22:41:43 +0200

    Добавьте файлы проекта.

---
+++ Project11.cpp
@@ -0,0 +1,84 @@
+﻿#include "Windows.h"
+#include "windowsx.h"
+#include "tchar.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WCHAR className[] = L"FirstWindow";
+
+	WNDCLASSEX wndClass;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = NULL;
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = className;
+
+	wndClass.lpfnWndProc = WindowProc;
+
+	if (!RegisterClassEx(&wndClass)) {
+		return 0;
+	}
+
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"My first window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, //x,y
+		CW_USEDEFAULT, CW_USEDEFAULT, //width, height
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_CREATE:
+		ShellExecute(NULL, TEXT("open"), TEXT("calc.exe"), NULL, NULL, SW_SHOWNORMAL);
+		break;
+
+	case WM_LBUTTONDOWN:
+	{
+		HWND calc = FindWindow(TEXT("CalcFrame"), TEXT("Калькулятор"));
+		if (calc)
+		{
+			SetWindowText(calc, TEXT("My text for calc"));
+		}
+
+		break;
+	}
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	return 0;
+}
